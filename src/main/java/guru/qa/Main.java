7106b0e089047f62a97c1e460a57c69e8875448a
Tombervilt <bad_hippie@mail.ru>
2025-04-09 01:34:11 +0300

    added getMassage

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,6 +2,10 @@ package guru.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello new branch1");
+        System.out.println(getMassage());
     }
+
+   private static String getMassage() {
+        return "Hello new branch1";
+   }
 }
