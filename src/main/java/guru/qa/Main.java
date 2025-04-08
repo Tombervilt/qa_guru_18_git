d62cfdaa6974ca16f58535a98b034ac7184121a9
TombervilT <38683935+Tombervilt@users.noreply.github.com>
2025-04-09 01:36:18 +0300

    Update Main.java

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,7 +2,7 @@ package guru.qa;
 
 public class Main {
     
-    privat static String msg = "Kilroy was here";
+    private static String msg = "Kilroy was here";
     
     public static void main(String[] args) {
         System.out.println("msg");
